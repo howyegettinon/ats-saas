a8f09409d590d783ab82502ab397c1c525dcbc7a
howyegettinon <howyegettinon@gmail.com>
2025-02-16 18:38:27 +0000

    Update route.ts

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,7 @@
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 
-export default NextAuth({
+const options = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
@@ -19,4 +19,8 @@ export default NextAuth({
     },
   },
   secret: process.env.NEXTAUTH_SECRET,
-});
+};
+
+const authHandler = (req, res) => NextAuth(req, res, options);
+
+export { authHandler as GET, authHandler as POST };
