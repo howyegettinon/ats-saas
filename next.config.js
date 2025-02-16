7f5c3c25e6a0fda244bf49058bc3a3cbfb451e69
howyegettinon <howyegettinon@gmail.com>
2025-02-16 19:43:08 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
       source: '/(.*)',
       headers: [
         { key: 'X-Frame-Options', value: 'DENY' },
-        { key: 'Content-Security-Policy', value: "default-src 'self'" }
+        { key: 'Content-Security-Policy', value: "default-src 'self'; style-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-inline'" }
       ],
     },
   ],
