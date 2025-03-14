410a2cf7318fed12566bc13eaef65ada63a57811
howyegettinon <howyegettinon@gmail.com>
2025-03-14 13:56:58 +0000

    Update providers.tsx

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { ReactNode } from 'react'
-import AuthProvider from '@/components/AuthProvider'
+import AuthProvider from '../components/AuthProvider'
 
 interface ProvidersProps {
   children: ReactNode
