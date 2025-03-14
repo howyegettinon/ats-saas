0b4d55d9f825050abde068c1ac5f45e7aa6ca34b
howyegettinon <howyegettinon@gmail.com>
2025-03-14 14:08:27 +0000

    Update auth.ts

--- src/auth/auth.ts
+++ src/auth/auth.ts
@@ -1,8 +1,20 @@
 import { NextAuthOptions } from 'next-auth'
 import { PrismaAdapter } from '@next-auth/prisma-adapter'
 import { prisma } from './prisma'
+import GoogleProvider from 'next-auth/providers/google'
+import EmailProvider from 'next-auth/providers/email'
 
 export const authOptions: NextAuthOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID || '',
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
+    }),
+    EmailProvider({
+      server: process.env.EMAIL_SERVER || '',
+      from: process.env.EMAIL_FROM || '',
+    }),
+  ],
   adapter: PrismaAdapter(prisma),
   session: {
     strategy: 'jwt',
@@ -32,4 +44,5 @@ export const authOptions: NextAuthOptions = {
       return token
     },
   },
+  secret: process.env.NEXTAUTH_SECRET,
 }
